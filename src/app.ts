54505f20c1b55d5c23d299b9532d1c0b84e4e35a
DareenJO <dareen.alomari@gmail.com>
2022-11-30 08:48:51 +0300

    new

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import  express  from "express";
 import schoolmanage from './routs/schoolmanage'
+import auth from './routs/auth.router'
 import 'dotenv/config';
 import { connectDB } from './config/db';
 const app = express();
@@ -9,7 +10,8 @@ const app = express();
 //middleware 
 
 app.use(express.json());
-app.use('/api/v1/bookmanage', schoolmanage);
+app.use('/api/v1/schoolmanage', schoolmanage);
+app.use('/api/v1/authmanage', auth);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
